b691f72db3e9eb6db9a5ea237987906dc8375eea
samueldob <frankdobisamuel@gmail.com>
2023-07-19 13:10:01 +0100

    removed unused code

--- script.js
+++ script.js
@@ -164,20 +164,3 @@ decimalEl.addEventListener('click', () => {
     setStrAsValue(currentValueStr + '.');
   }
 });
-
-
-// Set up the time
-// const updateTime = () => {
-//   const currentTime = new Date();
-
-//   let currentHour = currentTime.getHours();
-//   const currentMinute = currentTime.getMinutes();
-
-//   if (currentHour > 12) {
-//     currentHour -= 12;
-//   }
-//   hourEl.textContent = currentHour.toString();
-//   minuteEl.textContent = currentMinute.toString().padStart(2, '0');
-// }
-// setInterval(updateTime, 1000);
-// updateTime();
